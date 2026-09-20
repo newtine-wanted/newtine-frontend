739f2d4ca935b14efe421ad8dc269326e30b7237
sea-ya <seah@nano.seoul.kr>
2026-09-20 15:54:27 +0900

    chore(my-page): 동작하지 않는 메뉴 버튼 제거

--- src/features/my-page/index.ts
+++ src/features/my-page/index.ts
@@ -1,5 +1,4 @@
 export { MyPageContent } from "./my-page-content";
-export { MyPageMenuButton } from "./menu-button";
 export { LikedNewsCount } from "./liked-news/liked-news-count";
 export { LikedNewsList } from "./liked-news/liked-news-list";
 export { LikedNewsProvider } from "./liked-news/liked-news-provider";
